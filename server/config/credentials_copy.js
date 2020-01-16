671e4cfe938b6086804f1dc1241ef3573d1d812a
x3lq <webmaster@x3lq.de>
2020-01-16 20:13:44 +0100

    adding credentials script and global dbConnector

---
+++ server/config/credentials_copy.js
@@ -0,0 +1,23 @@
+var credentials = {
+    development: {
+        //mongodb connection settings
+        database: {
+            host:   '127.0.0.1',
+            port:   '27017',
+            db:     '',
+            user:   '',
+            password:   ''
+        },
+    },
+    production: {
+        //mongodb connection settings
+        database: {
+            host: '127.0.0.1',
+            port: '27017',
+            db:     '',
+            user:   '',
+            password:   ''
+        },
+    }
+    };
+    module.exports = credentials;
