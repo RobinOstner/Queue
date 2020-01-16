5b62b7283e519fb1d9277486d9c3113b3724cca5
x3lq <webmaster@x3lq.de>
2020-01-16 21:13:43 +0100

    moving login url to credentials

--- server/util/dbConnection.js
+++ server/util/dbConnection.js
@@ -7,22 +7,17 @@ const credentials = require('../config/credentials')[env]
 
 class DBConnection {
     static connectToMongo() {
-        console.log("Trying to connect to db")
         if ( this.db ) return Promise.resolve(this.db)
-        return MongoClient.connect(this.url, this.options, function(err, db) {
-            if(err) {
-                console.log("error occured" + err);
-            } else {
-                console.log("Connected to DB")
-                this.db = db;
-            }
-        })
+        return MongoClient.connect(credentials.database.url, this.options)
+            .then(db => this.db = db)
+            .catch(err => {console.log(err)});
     }
 }
 
 DBConnection.db = null
-DBConnection.url = 'mongodb://' + credentials.database.user + ':' + credentials.database.password +
-    '@'+ credentials.database.host + ':' + credentials.database.port + '/' + credentials.database.name;
+//DBConnection.url = 'mongodb+srv://' + credentials.database.user +':' + credentials.database.password + '@queue-xdyvz.mongodb.net/test?retryWrites=true&w=majority'
+//DBConnection.url = 'mongodb://' + credentials.database.user + ':' + credentials.database.password + '@'+ credentials.database.host + ':' + credentials.database.port + '/' + credentials.database.name;
+//DBConnection.urlNoAuth = 'mongodb://' + credentials.database.host + ':' + credentials.database.port + '/' + credentials.database.name;
 DBConnection.options = {
     bufferMaxEntries:   0,
     reconnectTries:     5000,
