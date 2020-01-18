c05a73f41a120fda22ebe187614cbeb6aaf68632
x3lq <webmaster@x3lq.de>
2020-01-18 17:34:53 +0100

    improving structure and authorization header checking

--- server/util/token/tokenChecker.js
+++ server/util/token/tokenChecker.js
@@ -2,17 +2,14 @@ const fs = require('fs');
 const jwt = require('jsonwebtoken');
 
 var env = process.env.NODE_ENV || 'development';
-const credentials = require('./../config/credentials')[env];
+const credentials = require('./../../config/credentials')[env];
 
 const tokenKey = fs.readFileSync('./server/config/keys/jwt_key.pem', 'utf8');
 
 module.exports = (req, res, next) => {
 	let token = req.body.token || req.query.token || req.headers['x-access-token'] || req.headers.authorization;
 
-	console.log(req);
-	console.log(token);
-
-	if(!token) {
+	if(!token || !token.startsWith('Bearer ')) {
 		return res.status(403).send({
 			"error": true,
 			"message": 'No token provided.'
@@ -23,7 +20,6 @@ module.exports = (req, res, next) => {
 
 	jwt.verify(token, tokenKey, function(err, decoded) {
 		if (err) {
-			console.log(err);
 			return res.status(401).json({"error": true, "message": 'Unauthorized access.' });
 		}
 		req.decoded = decoded;
