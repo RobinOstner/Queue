ec2114301ff4db35d708c8b7f1ac5fc697667038
Robin Ostner <Ostner_Robin@web.de>
2020-03-24 00:21:13 +0100

    Mobile Guest Queue

---
+++ client/src/components/Queue/Mobile/index.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class="scroll">
+    <div class="spacer"/>
+    <transition-group name="tracks">
+      <preview v-for="(track, index) in tracks" :key="track.id" :id="track.id" :title="track.title" :artist="track.artist" :votes="track.votes" :duration="track.duration" :coverURL="track.coverURL" :spacer="showSpacer(index)" />
+    </transition-group>
+    <div class="spacer"/>
+    <div class="spacer"/>
+  </div>
+</template>
+
+<script>
+  import api from "@/api";
+
+  import Preview from "./Preview";
+  import { RetryError } from "@/exception/retryError";
+  import { mapGetters } from "vuex";
+  import { mapMutations } from "vuex";
+
+  export default {
+    name: "mobile-queue",
+    components: {
+      Preview
+    },
+    data: function() {
+      return {
+        tracks: [],
+        refreshTimer: ""
+      };
+    },
+    computed: {
+      ...mapGetters({
+        refreshTime: "queue/getRefreshTime",
+        queueID: "queue/getQueueID",
+        votedTracks: "queue/getVotedTracks"
+      }),
+    },
+    created() {
+      this.refresh();
+      this.refreshTimer = setInterval(this.refresh, this.refreshTime);
+    },
+    methods: {
+      ...mapMutations({
+        removeVotedTrack: "queue/REMOVE_VOTED_TRACK"
+      }),
+      showSpacer: function(index) {
+        return index != this.tracks.length - 1;
+      },
+      refresh: async function() {
+        if (!this.queueID) {
+          return;
+        }
+
+        api.queue
+          .getTracks(this.$store, 0, 20)
+          .then(res => {
+            if (res.data.tracks) {
+              this.tracks = res.data.tracks;
+              this.cleanVotedTracks();
+            }
+          })
+          .catch(err => {
+            if (err instanceof RetryError) {
+              api.queue.getTracks(this.$store, 0, 20).then(res => {
+                if (res.data.tracks) {
+                  this.tracks = res.data.tracks;
+                }
+              });
+            }
+          });
+      },
+      cleanVotedTracks: function() {
+        var voted = this.votedTracks;
+
+        this.votedTracks.forEach(votedTrack => {
+          var included = false;
+
+          for (var i = 0; i < this.tracks.length; i++) {
+            if (this.tracks[i].id == votedTrack) {
+              included = true;
+              break;
+            }
+          }
+
+          if (!included) {
+            this.removeVotedTrack(votedTrack);
+          }
+        });
+      }
+    },
+    beforeDestroy() {
+      clearInterval(this.refreshTimer);
+    }
+  };
+</script>
+
+<style lang="scss" scoped>
+  .spacer {
+    height: 5.5em;
+  }
+
+  .tracks-move {
+    transition: transform 1s;
+  }
+
+  .scroll {
+    overflow: auto;
+    height: 100%;
+  }
+  
+  .scroll::-webkit-scrollbar {
+    width: 0px;
+  }
+</style>
--- client/src/views/Guest/GuestMobile.vue
+++ client/src/views/Guest/GuestMobile.vue
@@ -1,16 +1,169 @@
 <template>
-  <div>
-
+  <div class="guest">
+    <div class="header">
+      <h1 class="title left">Queue</h1>
+      <h1 class="title right">{{ queueID }}</h1>
+      <div class="trackInfo">
+        <h1 class="trackTitle">{{ currentTrack.title }}</h1>
+        <div class="separator"></div>
+        <h1 class="trackArtist">{{ currentTrack.artist }}</h1>
+      </div>
+    </div>
+    <div class="content">
+      <mobile-queue />
+      <div class="searchBar">
+        <div class="searchButton">
+          <div class="container">
+            <img src="/Icons/SearchIcon.png" alt="Search Icon" class="icon" />
+            <h2 class="search">Search</h2>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: "Guest",
-  
-}
+  import MobileQueue from "@/components/Queue/Mobile";
+
+  export default {
+    name: "Guest",
+    components: {
+      MobileQueue
+    },
+    props: {
+      queueID: Number,
+      currentTrack: Object
+    }
+  };
 </script>
 
 <style lang="scss" scoped>
+  .guest {
+    background-image: linear-gradient(35deg, #060c14 30%, #1a3c5c 90%);
+    height: 100%;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    color: white;
+  }
 
+  .header {
+    position: relative;
+    overflow: hidden;
+    padding: 5px 5px 10px;
+    z-index: 1;
+    box-shadow: 0 10px 10px rgba(0, 0, 0, 0.25);
+    background-color: rgba(8, 65, 83, 0.15);
+    backdrop-filter: blur(20px);
+
+    .title {
+      margin: 0;
+      color: white;
+      font-family: "Rationale", sans-serif;
+      font-size: 1.2em;
+      font-weight: normal;
+      text-transform: uppercase;
+      z-index: 2;
+    }
+
+    .left {
+      float: left;
+    }
+
+    .right {
+      float: right;
+    }
+
+    .trackInfo {
+      width: 100%;
+      text-align: center;
+      margin: 0;
+      margin-top: 15px;
+      align-content: center;
+      z-index: 2;
+
+      .trackTitle {
+        margin: 0;
+        font-size: 1.5em;
+      }
+
+      .separator {
+        height: 1px;
+        width: 75%;
+        background-color: white;
+        margin: 5px auto;
+      }
+
+      .trackArtist {
+        margin: 0;
+        font-size: 1.2em;
+      }
+    }
+  }
+
+  .center {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+  }
+
+  .content {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    padding: 0 0 0 0;
+    flex-grow: 1;
+
+    host-queue {
+      width: 100%;
+    }
+
+    .searchBar {
+      position: absolute;
+      bottom: 0;
+      background-image: linear-gradient(0deg, #060c14 25%, #060c1400);
+      height: 35%;
+      width: 100%;
+      pointer-events: none;
+
+      .searchButton {
+        position: absolute;
+        bottom: 10%;
+        left: 0;
+        right: 0;
+        margin: auto;
+        width: 80%;
+        height: 2.5em;
+        border-style: solid;
+        border-width: 1px;
+        border-color: white;
+        text-align: center;
+        display: flex;
+        flex-grow: 0;
+
+        .container {
+          width: 100%;
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+          justify-content: center;
+
+          .icon {
+            height: 35%;
+            width: auto;
+          }
+
+          h2 {
+            margin: 5px;
+            font-weight: normal;
+            font-size: 1em;
+          }
+        }
+      }
+    }
+  }
 </style>
