fe10049d2f5cebdc0a9030f533b26abc66d0b8d1
flspr <florian.sprang@x3lq.de>
2020-03-26 17:03:07 +0100

    removing old server

--- server/util/token/tokenHandler.js
+++ server/util/token/tokenHandler.js
@@ -1,26 +1,24 @@
-const fs = require('fs')
 const jwt = require('jsonwebtoken');
 
-var env = process.env.NODE_ENV || 'development';
-const credentials = require('./../../config/credentials')[env];
-
-const tokenKey = credentials.jwt.hostAccessTokenSecret;
-const refreshKey = credentials.jwt.hostRefreshTokenSecret;
-
 module.exports = {
-	createInitialTokenSet: function (salt) {
-		let tokenSet = {
-			"token": createToken(tokenKey + salt, credentials.jwt.tokenLife),
-			"refreshToken": createToken(refreshKey + salt, credentials.jwt.refreshTokenLife)
-		};
-
-		return tokenSet;
+	createRefreshTokenClient: function (salt) {
+		return createToken(process.env.CLIENT_REFRESH_TOKEN_SECRET + salt, process.env.REFRESH_TOKEN_LIFE)
 	},
+	createRefreshTokenHost: function (salt) {
+		return createToken(process.env.HOST_REFRESH_TOKEN_SECRET + salt, process.env.REFRESH_TOKEN_LIFE)
+	},
+	signClientData: function(salt, data) {
+		return signData(process.env.CLIENT_ACCESS_TOKEN_SECRET + salt, data);
+	},
+	signHostData: function(salt, data) {
+		return signData(process.env.HOST_ACCESS_TOKEN_SECRET + salt, data);
+	}
 };
 
 function createToken(key, expTime) {
-	let token = jwt.sign({exp: Math.floor(Date.now() / 1000) + expTime},
-		key, {algorithm: 'HS256'});
+	return jwt.sign({exp: Math.floor(Date.now() / 1000) + parseInt(expTime)}, key);
+}
 
-	return token;
+function signData(key, data) {
+	return jwt.sign(data, key, {expiresIn: parseInt(process.env.TOKEN_LIFE)});
 }
